7ec7064c0f63a19042a5d7f34013fc38c617bd36
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-24 01:12:29 +0600

    find all parts data

--- index.js
+++ index.js
@@ -8,6 +8,42 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json());
 
+// bay-admin
+// KqgD6FctvwBJu45G
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v2wyj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+//all curd operation is here
+async function run() {
+    try {
+        await client.connect();
+
+        const partsCollection = client.db("parts-collection").collection("parts");
+
+
+        app.get('/parts', async (req, res) => {
+            const query = {}
+            const cursor = partsCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+    } finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send('Manufacturing server working')
 })
