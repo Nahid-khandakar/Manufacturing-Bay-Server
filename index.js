e0e01abca232a6efdc70aa19ed87665ce99c7123
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-25 02:32:25 +0600

    update all

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
 
@@ -18,7 +19,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+//check token
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization
 
+    if (!authHeader) {
+        res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        //console.log('come form decodec', decoded)
+        req.decoded = decoded
+        next()
+    });
+}
 
 
 //all curd operation is here
@@ -28,8 +46,10 @@ async function run() {
 
         const partsCollection = client.db("mfg-bay").collection("parts-collection");
         const orderPartsCollection = client.db("mfg-bay").collection("order-collection");
+        const userCollection = client.db("mfg-bay").collection("users");
 
 
+        //get all parts in home section
         app.get('/parts', async (req, res) => {
             const query = {}
             const cursor = partsCollection.find(query);
@@ -37,13 +57,29 @@ async function run() {
             res.send(result)
         })
 
-
+        //get purchase data for purchase page
         app.get('/purchase/:id', async (req, res) => {
             const id = req.params
             const query = { _id: ObjectId(id) }
             const result = await partsCollection.findOne(query)
             res.send(result)
         })
+
+
+        app.get('/orders', verifyJwt, async (req, res) => {
+            const purchaseEmail = req.query.purchaseEmail
+            const decodedEmail = req.decoded.email
+
+            if (purchaseEmail === decodedEmail) {
+                const query = { purchaseEmail: purchaseEmail }
+                const orders = await orderPartsCollection.find(query).toArray()
+                return res.send(orders)
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden' })
+            }
+        })
+
 
 
         app.put('/parts/:id', async (req, res) => {
@@ -64,6 +100,24 @@ async function run() {
 
             res.send(updateDoc)
 
+        })
+
+
+        //put all user information && update a document
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+
+            //token send
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '6h' })
+            res.send({ result, token: token })
         })
 
 
